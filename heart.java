8e25731ca26b844ccc1a90e6b38a9d8e8f448ef2
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-10 13:01:55 +0530

    Create heart.java

---
+++ heart.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Heart{
+    public static void main (String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size oh heart");
+        int n=sc.nextInt();
+        for(int i=-3*n/2;i<=n;i++){
+            for(int j=-3*n/2;j<3*n/2;j++){
+                if((Math.abs(i)+Math.abs(j)<n)||((-n/2-i)*(-n/2-i))+(n/2-j)*(n/2-j)<=n*n/2||((-n/2-i)*(-n/2-i))+(-n/2-j)*(-n/2-j)<=n*n/2){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
