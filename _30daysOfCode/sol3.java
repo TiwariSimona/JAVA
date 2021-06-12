2d77f03a4244e6cc4a262e3fbf33d9412b68a7cd
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-12 10:43:11 +0530

    Create sol3.java

---
+++ _30daysOfCode/sol3.java
@@ -0,0 +1,1 @@
+//Given two long integers numbers min and max find and print a typical part n/d to such an extent that min<=d<=max and |n/d-pi| is negligible if there are a few divisions having an insignificant distance to pi pick the one with the littlest denominator.
