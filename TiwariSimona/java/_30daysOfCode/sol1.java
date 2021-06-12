4808563fb01b03b725dc5f38cd79f5ceeb3564fc
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-12 10:07:44 +0530

    Create sol1.java

---
+++ TiwariSimona/java/_30daysOfCode/sol1.java
@@ -0,0 +1,24 @@
+//From a set of permutations for a given number n find the number of elements such hat no element appears in its original position.
+
+package daysOfCode;
+import java.util.*; 
+public class sol_1 {
+	
+	  public static void main (String[] args){
+    Scanner sc = new Scanner(System.in);
+  	System.out.println("Enter a positive integer");
+    int n =sc.nextInt();
+	  System.out.println( "The number of elements such hat no element appears in its original position " +total(n));                     
+	    }
+	    
+	    static int total(int num){
+	         if (num == 1) 
+	        	return 0;
+	        else if (num == 2) 
+	        	return 1;
+	        else
+	            return (num - 1) * (total(num- 1) +  total(num - 2));
+	                         
+	      }
+	    }
+
