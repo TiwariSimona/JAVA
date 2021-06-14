0d95fe3f6285b81921b99cf41e7460b5383a3d54
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-14 12:34:02 +0530

    Update sol7.java

--- _30daysOfCode/sol7.java
+++ _30daysOfCode/sol7.java
@@ -3,24 +3,18 @@
 package daysOfCode;
 import java.io.*;
 import java.util.Scanner;
+
 public class sol_7 {
-	
- static double PI = 3.14;
-	     
+	 static double PI = 3.14;   
 	    static double areaOfInscribedCircle(float a)
 	    {
 	        return ( PI / 4 ) * a * a;
-	    }
-	 
+	    }	 
 	    public static void main (String[] args)
 	    {
 	    	Scanner sc= new Scanner(System.in);
 	    	System.out.println("enter side of square");
-	        float a = sc.nextFloat();
-	          System.out.println("Area of an inscribed" + " circle: " + areaOfInscribedCircle(a));
+	        float a = sc.nextFloat();     
+	        System.out.println("Area of an inscribed" + " circle: " + areaOfInscribedCircle(a));
 	    }
 	}
-
-
-
-
