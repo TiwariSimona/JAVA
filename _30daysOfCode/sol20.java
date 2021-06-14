fe3818d6dd61f0f17ff2c6877e8fde24cc055e52
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-14 16:24:13 +0530

    Update sol20.java

--- _30daysOfCode/sol20.java
+++ _30daysOfCode/sol20.java
@@ -3,10 +3,19 @@
 package daysOfCode;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 
 public class sol_20 {	
-	 public static void main(String args[]) throws IOException {
-      System.err.print("hello");		 
+ public static void main(String args[]) throws IOException {
+      OutputStreamWriter streamWriter = new OutputStreamWriter(System.out);
+      Scanner sc= new Scanner(System.in);
+      String str= sc.nextLine();
+      streamWriter.write(str);
+      streamWriter.flush();		 
 	   }
 	}
+
+
+      
+     
