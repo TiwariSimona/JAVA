6e2728206fb5fd65484ff72996cd888447fd857b
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-14 14:17:12 +0530

    Update sol8.java

--- _30daysOfCode/sol8.java
+++ _30daysOfCode/sol8.java
@@ -1,1 +1,14 @@
 //A truck can transport a maximum of 10000 kgs. Assume a heap of load containing 41 boxes should be shipped through the truck. The box weight of this sort of cargo follows a distribution with a mean of 210 kgs and standard deviation of 15 kgs. What is the probability that all 41 boxes can be loaded into the truck and transported?
+
+package daysOfCode;
+
+import java.util.Scanner;
+
+public class sol_8 {
+	
+ public static void main(String[] args) {
+ float x =-7/5;
+
+  System.out.printf("The value is " + x);
+ }
+}
