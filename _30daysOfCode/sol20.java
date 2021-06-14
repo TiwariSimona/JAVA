2d11364f1d410c4c934f657a8a859f3b758aa2e5
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-14 12:41:28 +0530

    Create sol20.java

---
+++ _30daysOfCode/sol20.java
@@ -0,0 +1,12 @@
+//Program to print a statement without using the print statement.
+
+package daysOfCode;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+
+public class sol_20 {	
+	 public static void main(String args[]) throws IOException {
+      System.err.print("hello");		 
+	   }
+	}
