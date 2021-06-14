04062312ea04dd09f3d154b80b5a5cee2099ba01
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-14 12:35:21 +0530

    Update sol9.java

--- _30daysOfCode/sol9.java
+++ _30daysOfCode/sol9.java
@@ -4,7 +4,6 @@ package daysOfCode;
 import java.util.*;
 
 public class sol_9 {
-	
 	static ArrayList<Integer> v=new ArrayList<Integer>();
 	static void multiply(int x)
 	{
@@ -13,6 +12,7 @@ public class sol_9 {
 	 for (int i = 0 ; i < size ; i++)
 	 {
 	     int res = carry + v.get(i) * x;
+
 	     v.set(i,res % 10);
 	     carry = res / 10;
 	 }
@@ -22,23 +22,22 @@ public class sol_9 {
 	     carry /= 10;
 	 }
 	}
-
 	static int findSumOfDigits(int n)
 	{
-	 v.add(1); // adds 1 to the end
+	 v.add(1);
+	
 	 for (int i=1; i<=n; i++)
 	     multiply(i);
+
 	 int sum = 0;
 	 int size = v.size();
 	 for (int i = 0 ; i < size ; i++)
 	     sum += v.get(i);
 	 return sum;
 	}
-
 	public static void main(String[] args)
 	{
 	 int n = 1000;
 	 System.out.println(findSumOfDigits(n));
 	}
-
 }
