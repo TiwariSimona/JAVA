910bfeff4ded6310610cb3d6099644aa53814fd0
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-14 14:21:07 +0530

    Update sol4.java

--- _30daysOfCode/sol4.java
+++ _30daysOfCode/sol4.java
@@ -9,14 +9,6 @@ public class sol_4 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int n = sc.nextInt();
-        int n_squ = n*n;  
- 
-        String str_n = Integer.toString(n);  
-        String squr = Integer.toString(n_squ);  
- 
-        if(squr.endsWith(str_n))  
-            System.out.println("The number is a vertex of rectangle");
-        else
-            System.out.println("The number is not a vertex of rectangle");
+       
     }
 }
