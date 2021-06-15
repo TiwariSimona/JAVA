a4c59e4f35244d376458b3b3f90576eb768add90
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-15 11:36:55 +0530

    Create sol75.java

---
+++ _30daysOfCode/sol75.java
@@ -0,0 +1,20 @@
+//WAP that show double factorial
+
+package daysOfCode;
+import java.util.Scanner;
+public class sol_75 {		 
+	    static long D_fac(long n)
+	    {
+	        if (n == 0 || n==1)
+	            return 1;            
+	        return n * D_fac(n - 2);
+	    }   
+	     public static void main (String[] args)
+	    {
+	    	 Scanner sc=new Scanner(System.in);
+	    	 System.out.println("Enter the number");
+	    	 int n= sc.nextInt();
+	        System.out.println("Double factorial is "+ D_fac(n));
+	            
+	    }
+	}
