9b840178213db2ee5a4676fb35515767487b0f09
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-15 11:40:11 +0530

    Create sol85.java

---
+++ _30daysOfCode/sol85.java
@@ -0,0 +1,25 @@
+//Print the following pattern.
+
+package daysOfCode;
+import java.util.Scanner;
+public class sol_85 {
+	      public static void main(String args[])  
+	       {  
+	    	  Scanner sc=new Scanner(System.in);
+	    	  System.out.println("Enter the number of rows");
+	          int row=  sc.nextInt();
+	            for (int i= row; i>= 1; i--)  
+	              {  
+	                  for (int j=row; j>i;j--)  
+	                   {  
+	                      System.out.print(" ");  
+	                   }  
+	                   for (int k=1;k<=i;k++)  
+	                     {  
+	                        System.out.print(k);
+	                     }  
+	                  System.out.println("");  
+	              }
+	       }
+	}
+
