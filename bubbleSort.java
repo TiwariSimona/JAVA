461b98ffee7a8304d734ed5909698a4f786f75ea
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-16 14:38:39 +0530

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,1 +1,19 @@
-*
+
+  int a[]={2, 1, 8, -3, 6, 4, 12}
+  int n=a.length;
+  for(int i=0;i<n-1;i++)
+  {
+    boolean sorted = true;
+    for(int j=0;j<n-1-1;j++)
+    {
+      if (a[j+1]<a[j])
+      {
+        int temp=a[j+1];
+        a[j+1]=a[j];
+        a[j]=temp;
+      }
+    }
+    sorted = false;
+    if(sorted)=break;
+  }
+     
