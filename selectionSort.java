1eaef0809230ea08ad275d4a90f8abd583bbf438
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-18 11:21:27 +0530

    Update selectionSort.java

--- selectionSort.java
+++ selectionSort.java
@@ -1,2 +1,16 @@
 
-int a[]=
+int a[]={3, 1, -2, 7, 4, 0};
+int n= a.length;
+for(int i=0;i<n-1;i++){
+  int minInd=i;
+  for(int j=i;j<n;j++){
+    if(a[j]<(a[minInd]){
+      minInd=j;
+    }
+       }
+       int temp=a[i]
+       a[i]=a[minInd];
+       a[minInd]=temp;
+       }
+       for(int:a){
+         System.out.println(e+" ");
