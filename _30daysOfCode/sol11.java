659588fcc32191fd1327ebe1a756f497740884fb
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-22 17:03:35 +0530

    Update sol11.java

--- _30daysOfCode/sol11.java
+++ _30daysOfCode/sol11.java
@@ -9,8 +9,6 @@ package daysOfCode;
 			Scanner sc=new Scanner(	System.in);
 	        System.out.println(" Enter the total no. of paths you want to compare for :");
 			int n=sc.nextInt();
-
-			
 			int xi,yi,xf,yf;
 			double d2=0;
 			double ar[]=new double[n];
@@ -18,15 +16,13 @@ package daysOfCode;
 			{
 	            System.out.println("Enter the constraints for "+(i+1)+" path");
 	            System.out.println("Enter X and Y coordinates of Initial position:");  
-				xi=sc.nextInt();
+				 xi=sc.nextInt();
 				 yi=sc.nextInt();
 	             System.out.println("Enter X and Y coordinates of Final position:");
 				 xf=sc.nextInt();
-				yf=sc.nextInt();
-				
+				 yf=sc.nextInt();
 				double d1=Math.sqrt((Math.pow(Math.abs(xi-xf),2))+ (Math.pow(Math.abs(yi-yf),2)));
 		       ar[i]=d1;
-				
 				}
 			 d2=ar[0];
 			 int n1=0;
@@ -42,7 +38,6 @@ package daysOfCode;
 			System.out.println(" You should go with path: " +(n1+1));
 			sc.close();
 		}
-
 	}
 
 
