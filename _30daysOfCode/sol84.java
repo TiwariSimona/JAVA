7a37e469d9c3c7dd0991cc2f84a97f55a2104376
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-24 10:24:08 +0530

    Update sol84.java

--- _30daysOfCode/sol84.java
+++ _30daysOfCode/sol84.java
@@ -1,1 +1,22 @@
-*
+//Print the following pattern.
+
+package daysOfCode;
+public class sol_84 {
+    public static void main(String args[])
+    {
+        int t=5;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=t;j++)
+            {
+                System.out.print("    ");}
+                for(int z=1;z<=i;z++)
+                {
+                    System.out.print(z+"       ");
+
+                }
+                System.out.println();
+                t--;
+            }
+        }
+    }
