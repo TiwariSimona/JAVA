89efbc57b1adb52b6cf68ba1bb1145bc8e7235c0
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-07-02 15:28:35 +0530

    Update sol54.java

--- _30daysOfCode/sol54.java
+++ _30daysOfCode/sol54.java
@@ -1,9 +1,7 @@
 //WAP to Print the pattern given below:
 
 package daysOfCode;
-
 public class sol_54 {
-	
 	    public static void main(String args[])
 	    {
 	        int m=5;
@@ -12,8 +10,7 @@ public class sol_54 {
 	        {System.out.print(s);
 	            for(int j=1;j<=m;j++ )
 	            {
-	            System.out.print("*");
-	           
+	            System.out.print("*");     
 	           }
 	           m-=2;
 	           System.out.println();
