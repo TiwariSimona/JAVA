83db9ad217861a9f3a3f2da6c08974d8b1de3136
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-09-03 12:47:20 +0530

    Create sol78.java

---
+++ _30daysOfCode/sol78.java
@@ -0,0 +1,24 @@
+//Given an array a[ ] of size n, swap the p th element from beginning with p th element from end.
+
+import java.util.*;
+public class Q78 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter array size: ");
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		System.out.println("Enter elements:");
+		for(int i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		System.out.print("Enter position: ");
+		int p=sc.nextInt();
+		sc.close();
+		int temp=a[p-1];
+		a[p-1]=a[a.length-p];
+		a[a.length-p]=temp;
+		System.out.println("Array after swapping:");
+		for(int e:a) {
+			System.out.print(e+" ");
+		}
+	}
+}
